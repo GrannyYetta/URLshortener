07f73011ae5c6d62a4d1c90eed82ae24d78576a1
GrannyYetta <you@example.com>
2024-01-09 15:11:13 +0100

    Contollers to be completed

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const connection = async () => {
 	try {
-		const connection = await mongoose.connect(process.env.DB_URI);
+		const connection = await mongoose.connect(process.env.DB_URI); // connecting to mongoDB
 		console.log(connection);
 	} catch (error) {
 		console.log(error);
